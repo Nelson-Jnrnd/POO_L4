95525b3b1c30705633cc7ec43ede087e8987e366
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2021-10-14 14:48:05 +0200

    exe 2 and exe 3 with genericity

--- src/com/NelsonJnrnd/Int.java
+++ src/com/NelsonJnrnd/Int.java
@@ -1,4 +1,27 @@
 package com.NelsonJnrnd;
 
-public class Int {
+public class Int implements Comparable<Int>{
+    public void set_value(int _value) {
+        this._value = _value;
+    }
+
+    public int get_value() {
+        return _value;
+    }
+
+    @Override
+    public String toString() {
+        return "to_String() perso " + Integer.toString(_value);
+    }
+
+    private int _value;
+
+    public Int(int value) {
+        this._value = value;
+    }
+
+    @Override
+    public int compareTo(Int o) {
+        return Integer.compare(_value, o._value);
+    }
 }
--- src/com/NelsonJnrnd/Main.java
+++ src/com/NelsonJnrnd/Main.java
@@ -20,7 +20,21 @@ public class Main {
         }
         return sign * outputInt;
     }
-    public static void main(String[] args) {
+
+    public static <T extends Comparable<T>> void BubbleSort(ArrayList<T> inputArray) {
+        if (!inputArray.isEmpty()) {
+            for (int i = 0; i < inputArray.size() - 1; i++) {
+                for (int j = 1; j < inputArray.size() - i; j++) {
+                    if (inputArray.get(j).compareTo(inputArray.get(j - 1)) < 0) {
+                        Collections.swap(inputArray, j, j - 1);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void exe1(String[] args) {
         ArrayList<Integer> argsInt = new ArrayList<>();
         System.out.println("Args in string");
         try {
@@ -28,13 +42,53 @@ public class Main {
                 System.out.println(arg);
                 argsInt.add(StringToInt(arg));
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("Inputs are not numbers");
         }
-        Collections.sort(argsInt);
+        //Collections.sort(argsInt);
+        BubbleSort(argsInt);
         System.out.println("Args in int");
-        for (int i: argsInt) {
+        for (int i : argsInt) {
             System.out.println(i);
         }
+    }
+
+    public static void exe2() {
+        System.out.println("Test de la classe Int");
+        Int monInt = new Int(31);
+
+        System.out.println("Constructeur et get value: " + monInt.get_value());
+        monInt.set_value(13);
+        System.out.println("set value :" + monInt.get_value());
+    }
+
+    public static void exe3(String[] args) {
+        ArrayList<Int> argsInt = new ArrayList<>();
+        System.out.println("Args in string");
+        try {
+            for (String arg : args) {
+                System.out.println(arg);
+                argsInt.add(new Int(StringToInt(arg)));
+            }
+        } catch (Exception ex) {
+            System.out.println("Inputs are not numbers");
+        }
+        BubbleSort(argsInt);
+        System.out.println("Args in int");
+        for (Int i : argsInt) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Exe 1");
+        exe1(args);
+
+        System.out.println("\nExe 2");
+        exe2();
+
+        System.out.println("\nExe 3");
+        exe3(args);
+
     }
 }
